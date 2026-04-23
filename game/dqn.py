72e108ed0a50f0f8f3d2007c10afff6dd2c8bc8f
Dexter◥▶_◀◤ <335ishwar@gmail.com>
2026-04-23 19:03:07 +0530

    Artecture of DQN

---
+++ game/dqn.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as  nn
+
+class DQN(nn.Module):        
+
+    #input dim  ,output dim, hidden dim
+    def __init__(self,state_din=12,action_din=12,   hidden_din=256):
+        super(DQN,self).__init__()  
+        self.model=nn.Sequential(
+
+                    nn.Linear(state_din,hidden_din),
+                    nn.ReLu(),
+
+                    nn.Linear(hidden_din,action_din )
+                )
+        
+    def forward(self,x):
+        return  self.model(x)
